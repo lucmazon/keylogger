636083587fe4a50df7878210fd1e079c484c8e02
Luc Mazon <luc.mazon-prestataire@fia-net.com>
2015-02-04 13:11:43 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import keylogger
+import time
+
+now = time.time()
+done = lambda: time.time() > now + 600
+def print_keys(t, modifiers, keys): print "%r   %r" % (keys, modifiers)
+
+keylogger.log(done, print_keys)
